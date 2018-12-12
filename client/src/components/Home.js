2f8885b6b3affc3405c6975388dc04021852388b
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-12-11 17:58:47 -0700

    coinmarketcap api no longer free had to get api key and find workaround for current configuration

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Button } from 'semantic-ui-react';
+import axios from 'axios'
 
 class Home extends Component {
+  state = { coin: {}}
+
+  clicky = () => {
+    axios.post('/api/coins?coin=btc')
+      .then( res => { debugger })
+  }
+
+  clicky2 = () => {
+    axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=')
+      .then( res => { debugger })
+  }
   render() {
     return (
-      <Header as="h1" textAlign="center">Home Component</Header>
+      <>
+      <Button onClick={this.clicky}>
+      Click Me!
+      </Button>
+      <Button onClick={this.clicky2}>
+      coins
+      </Button>
+      </>
     );
   }
 }
